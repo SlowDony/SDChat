5020f8e5c93734267c5989412016da8a1ba0f0f9
slowdony <435574793@qq.com>
2017-08-07 21:58:51 +0800

    优化聊天

--- SDChat/SDChatViewController/SDViewControllers/SDChatDetailViewController.m
+++ SDChat/SDChatViewController/SDViewControllers/SDChatDetailViewController.m
@@ -6,6 +6,31 @@
 //  Copyright © 2017年 SlowDony. All rights reserved.
 //
 
+/*
+ 
+ 目前实现
+ 
+ 基本聊天对话业务,
+ 聊天对话UI布局,
+ 表情键盘弹出,
+ 支持emoji表情.
+ 待完成
+ 
+ 图文混排,
+ 表情键盘弹出的优化,
+ 支持png格式表情,
+ 拍照上传图片.
+ 未来
+ 
+ 完善SDChat
+ 我的邮箱:devslowdony@gmail.com
+ 项目更新地址 GitHub:https://github.com/SlowDony/SDChat
+ 如果有好的建议或者意见 ,欢迎指出 , 您的支持是对我最大的鼓励,谢谢. 求STAR ..😆
+ */
+
+
+
+
 #import "SDChatDetailViewController.h"
 
 /* 聊天内容模型 */
